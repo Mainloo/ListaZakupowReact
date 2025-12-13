dc72ee128981b0b5e60a4f7c5ad7ce83b55c45ec
bboychlodny <kacper.mroz1@gmail.com>
2025-12-13 17:49:35 +0100

    usuniecie bazy danych z projektu

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,11 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
-import connectDB from "../database/db"; 
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
-
-connectDB();
 
 interface Product {
     id: number;
